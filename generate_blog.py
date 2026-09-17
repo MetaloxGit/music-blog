6a11d0661319bf17971f74f8b33ef36ad0d85f38
MetaloxGit <metalox4@outlook.com>
2026-09-17 08:57:23 +0200

    Fix GitHub repository owner casing

--- generate_blog.py
+++ generate_blog.py
@@ -9,11 +9,11 @@ from datetime import datetime
 
 # Configurations
 # Détection automatique du dépôt sur GitHub Actions
-github_repo = os.getenv("GITHUB_REPOSITORY", "metaloxgit/music-records")
+github_repo = os.getenv("GITHUB_REPOSITORY", "MetaloxGit/music-records")
 if "/" in github_repo:
     REPO_OWNER, REPO_NAME = github_repo.split("/", 1)
 else:
-    REPO_OWNER = "metaloxgit"
+    REPO_OWNER = "MetaloxGit"
     REPO_NAME = "music-records"
 MIN_PRODUCTS = 1
 POSTS_DIR = "_posts"
